73edb7ab5c81bca6f73499b7d80b6f50f6d131de
itsuikolaptop <new.life.bumper@gmail.com>
2025-08-19 14:08:08 +0900

    sidebar.phpとfooter.php編集

--- footer.php
+++ footer.php
@@ -1,12 +1,23 @@
 <footer class="l-footer">
             <div class="p-footer">
+            <?php
+            if ( has_nav_menu( 'footer_nav' ) ) {
+                wp_nav_menu( array(
+                    'theme_location' => 'footer_nav',
+                    'container' => false,
+                    'menu_class' => 'p-footer__menu',
+                ) );
+            } else {
+            ?>    
             <div class="p-footer__menu">
-                <a href="/" class="p-footer__menu-link">ショップ情報</a>
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__menu-link">ショップ情報</a>
                     
-                <span class="p-footer__menu-link--line"></span>
+                <span class="p-footer__menu-link--line" aria-hidden="true"></span>
                     
-                <a href="/" class="p-footer__menu-link">ヒストリー</a>
-            </div>    
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="p-footer__menu-link">ヒストリー</a>
+            </div>
+            <?php } ?>
+            
             <small class="p-footer__copy">Copyright: RaiseTech</small>
             </div>
         </footer>
--- sidebar.php
+++ sidebar.php
@@ -1,41 +1,53 @@
-<aside class="l-sidebar js-sidebar">
-            <div class="p-sidebar js-sidebar">
+<aside class="l-sidebar js-sidebar" aria-hidden="true">
+            <div class="p-sidebar">
             <button class="p-sidebar__btn c-button js-menu">
                 <h2 class="p-sidebar__title">Menu</h2>
-            </button>    
+            </button>
+            
+            <?php
+            if ( has_nav_menu( 'sidebar_menu' ) ) {
+                wp_nav_menu( array(
+                    'theme_location' => 'sidebar_menu',
+                    'container' => false,
+                    'menu_class' => 'p-sidebar__nav',
+                ) );
+            } else {
+            // フォールバック（既存の静的メニュー）
+            ?>
             <nav class="p-sidebar__nav">
                 <section class="p-sidebar__nav--bargur">
                     <h3 class="p-sidebar__heading">バーガー</h3>
                     <ul class="p-sidebar__menu">
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">ハンバーガー</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">チーズバーガー</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">テリヤキバーガー</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">アボカドバーガー</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">フィッシュバーガー</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">ベーコンバーガー</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">チキンバーガー</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">ハンバーガー</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">チーズバーガー</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">テリヤキバーガー</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">アボカドバーガー</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">フィッシュバーガー</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">ベーコンバーガー</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">チキンバーガー</a></li>
                     </ul>
                 </section>
                 <section class="p-sidebar__nav--side">
                     <h3 class="p-sidebar__heading">サイド</h3>
                     <ul class="p-sidebar__menu">
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">ポテト</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">サラダ</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">ナゲット</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">コーン</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">ポテト</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">サラダ</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">ナゲット</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">コーン</a></li>
                     </ul>
                 </section>
                 <section class="p-sidebar__nav--drink">
                     <h3 class="p-sidebar__heading">ドリンク</h3>
                     <ul class="p-sidebar__menu">
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">コーラ</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">ファンタ</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">オレンジ</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">アップル</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">紅茶（Ice/Hot）</a></li>
-                        <li class="p-sidebar__list"><a href="/" class="p-sidebar__list-menu">コーヒー（Ice/Hot）</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">コーラ</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">ファンタ</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">オレンジ</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">アップル</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">紅茶（Ice/Hot）</a></li>
+                        <li class="p-sidebar__list"><a href="<?php echo esc_url(home_url('/')); ?>" class="p-sidebar__list-menu">コーヒー（Ice/Hot）</a></li>
                     </ul>
                 </section>
             </nav>
+            <?php } ?>
             </div>    
         </aside>
