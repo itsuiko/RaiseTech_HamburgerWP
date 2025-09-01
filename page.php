69f9cc70737070ce8a547b735875121c5db9f98f
itsuikolaptop <new.life.bumper@gmail.com>
2025-09-01 18:43:54 +0900

    page.php編集とサイドバーの表示修正

--- page.php
+++ page.php
@@ -1,115 +1,29 @@
 <?php get_header(); ?>
         <main class="l-main">
-            <div class="p-main">
-            <div class="p-mainvisual--singlepage">
-                <div class="p-mainvisual__img--page"></div>
-                <h1 class="p-mainvisual__heading--singlepage">ショップについて</h1>    
-            </div>
-            <div class="c-contents p-contents--singlepage">
-                <div class="p-textbox--singlepage">
-                    <h2 class="c-contents__heading p-textbox__heading--single-h2">見出しh2</h2>
-                    <p class="c-contents__text p-textbox__text--single">Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。</p>
-                    <h3 class="c-contents__heading p-textbox__heading--single-h3">見出しh3</h3>
-                    <h4 class="c-contents__heading p-textbox__heading--single-h4">見出しh4</h4>
-                    <h5 class="c-contents__heading p-textbox__heading--single-h5">見出しh5</h5>
-                    <h6 class="c-contents__heading p-textbox__heading--single-h6">見出しh6</h6>
-                </div>
-                <blockquote class="p-blockquote">
-                    <p class="p-blockquote__text">Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
-                    <span class="p-blockquote__label">出典元：</span>
-                    <a href="#" class="p-blockquote__link"><cite class="p-blockquote__cite">○○○○○○○○○○○○</cite></a>
-                </blockquote>
-                <div class="p-contents__img--singlepage">
-                    <img class="p-contents__img" src="/images/cooked-foods-750073.jpg" alt="Hamburger">
-                </div>
-                <div class="p-card--singlepage">
-                    <img class="p-card__img--singlepage" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <p class="p-card__text--singlepage">  テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストがテキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります </p>
-                </div>
-                <div class="p-card--singlepage2">
-                    <img class="p-card__img--singlepage" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <p class="p-card__text--singlepage">  テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストがテキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります </p>
-                </div>
-                <div class="p-contents__img2--singlepage">
-                    <img class="p-contents__img2" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                </div>
-                <div class="p-contents__imgbox">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                    <img class="p-contents__img" src="/images/cooked-foods2.jpg" alt="Hamburger">
-                </div>
-                <div class="p-contents__list-container">
-                    <div class="p-contents__list-box">
-                        <ol class="p-contents__list" start="1">
-                            <li class="p-contents__list-text">1.リストリストリストリストリストリスト</li> 
-                            <li class="p-contents__list-text">2.リストリストリストリストリストリスト</li> 
-                        </ol>
-                        <ol class="p-contents__list" start="1">
-                            <li class="p-contents__list-text">1.リスト2リスト2リスト2リスト2リスト2リスト2</li> 
-                            <li class="p-contents__list-text">2.リスト2リスト2リスト2リスト2リスト2リスト2</li> 
-                        </ol>
-                        <ol class="p-contents__list" start="1">
-                            <li class="p-contents__list-text">1.リストリストリストリストリストリスト</li> 
-                            <li class="p-contents__list-text">2.リストリストリストリストリストリスト</li> 
-                        </ol>
-                    </div>    
-                    <div class="p-contents__list-box">
-                        <ol class="p-contents__list">
-                            <li class="p-contents__list-text">リストリストリストリストリストリスト</li> 
-                            <li class="p-contents__list-text">リストリストリストリストリストリスト</li> 
-                        </ol>
-                        <ol class="p-contents__list">
-                            <li class="p-contents__list-text">リスト2リスト2リスト2リスト2リスト2リスト2</li> 
-                            <li class="p-contents__list-text">リスト2リスト2リスト2リスト2リスト2リスト2</li> 
-                        </ol>
-                        <ol class="p-contents__list">
-                            <li class="p-contents__list-text">リストリストリストリストリストリスト</li> 
-                            <li class="p-contents__list-text">リストリストリストリストリストリスト</li> 
-                        </ol>
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                <article <?php post_class(); ?>>
+                    <div class="p-mainvisual--singlepage">
+                        <?php
+                        $image_url = has_post_thumbnail()
+                            ? get_the_post_thumbnail_url(get_the_ID(), 'full')
+                            : get_template_directory_uri() . '/images/page-title_shop@2x.jpg';
+                        ?>
+                        <div class="p-mainvisual__img--single" style="background-image: url('<?php echo esc_url($image_url); ?>');"></div>
+                        <h1 class="p-mainvisual__heading--singlepage"><?php echo esc_html(get_the_title()); ?></h1>
                     </div>
-                </div>
-                <div class="p-contents__code-box">
-                    &lt;html&gt;<br>
-                    &nbsp;&nbsp;&nbsp;&lt;head&gt;<br>
-                    &nbsp;&nbsp;&nbsp;&lt;/head&gt;<br>
-                    &nbsp;&nbsp;&nbsp;&lt;body&gt;<br>
-                    &nbsp;&nbsp;&nbsp;&lt;/body&gt;<br>
-                    &lt;/html&gt;<br>
-                </div>
-                <table class="p-contents__table">
-                    <tr class="p-contents__tr">
-                        <th class="p-contents__th">テーブル</th>
-                        <td class="p-contents__td">テーブル</td>
-                    </tr>
-                    <tr class="p-contents__tr">
-                        <th class="p-contents__th">テーブル</th>
-                        <td class="p-contents__td">テーブル</td>
-                    </tr>
-                    <tr class="p-contents__tr">
-                        <th class="p-contents__th">テーブル</th>
-                        <td class="p-contents__td">テーブル</td>
-                    </tr>
-                    <tr class="p-contents__tr">
-                        <th class="p-contents__th">テーブル</th>
-                        <td class="p-contents__td">テーブル</td>
-                    </tr>
-                    <tr class="p-contents__tr">
-                        <th class="p-contents__th">テーブル</th>
-                        <td class="p-contents__td">テーブル</td>
-                    </tr>
-                </table>
-                <button class="c-button p-contents__btn">ボタン</button>
-                <p class="p-contents__text">boldboldboldboldboldboldbold</p>
-            </div>
-            
-            </div>
+
+                    <div class="c-contents p-contents--singlepage">
+                        <div class="p-textbox--singlepage">
+                            <?php the_content(); ?>
+
+                        </div>
+
+                    </div>
+                </article>
+
+            <?php endwhile; endif; ?>
         </main>
-        <?php get_sidebar(); ?>
-        <div class="p-overlay"></div>    
-        <?php get_footer(); ?>
+<?php get_sidebar(); ?>
+<div class="p-overlay"></div>    
+<?php get_footer(); ?>
